f9c7f7c659a5ed21613882e6341f28078cae1158
toast <smol@toast.cyou>
2022-02-04 18:40:47 -0500

    got sourceview running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use gtk::{TextBuffer, prelude::*};
+use glib::{clone, MainContext};
+use sourceview::*;
+
 fn main() {
-    println!("Hello, world!");
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    // init builder
+    let glade_src = include_str!("catboy.glade");
+    let builder = gtk::Builder::from_string(glade_src);
+
+    // init main window
+    let window: gtk::Window = builder.get_object("mainwindow").unwrap();
+
+    // init widgets (and mysterious sourceview incantation)
+    sourceview::View::static_type();
+    let text_output: gtk::TextView = builder.get_object("sourceview").unwrap();
+
+    let name_buf: gtk::TextBuffer = gtk::TextBuffer::new(None::<&gtk::TextTagTable>);
+    text_output.set_buffer(Some(&name_buf));
+
+    // window destructor (closes program properly)
+    window.connect_destroy( |_| { 
+        gtk::main_quit();
+    });
+
+    // init program
+    window.show_all();
+    gtk::main();
 }
+
+// :3
